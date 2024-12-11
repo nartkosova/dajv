fa1d362ea9e7f0cb9c7782fa93d4e4b300f5eb75
nartkosova <nartkosova21@gmail.com>
2024-12-11 10:22:48 +0100

    fixed about

--- src/components/AboutComponents/About.tsx
+++ src/components/AboutComponents/About.tsx
@@ -65,42 +65,27 @@ const About = () => {
         image="https://s3-alpha-sig.figma.com/img/2e0e/fafd/58d36059688776d4ba895dbd2789ce57?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bbYxFxmjWdQa6nfWl6B-svty16pv6I40ldGG-PYcr-fiKpWQZprxrJJc9VI2y9vEWe8FlkP8zdy7UkbUj9bLO9~kNJQbp1M1NBzy4fnQcHD1~Vrf7HWsa0jowmRejBQNsz77nhEwkgdxH8rTBugxA20xb-VB2w0M1qnjoSn3ISIJsRh1dEjm0cPZGLFCZtouBrE2CVyVK-ISRTrerE74Jax7KrBStLxAoj~GqUxKmnlGcfAS9t0cXk7Kf4YWdC~hj7XjCFj2sfYhN-TdOJ3V-gSMEZ1Y10UvwFM7EJk-qLyouo0idlNRMgq-FdTMZpPyCJ4KvSro-RcYoe5VZ~wJ4A__"
       />
 
-      <Box sx={{ padding: "4rem 2rem" }}>
-        <Typography
-          variant="h2"
-          sx={{
-            fontSize: { xs: "2rem", md: "2.5rem" },
-            fontWeight: "600",
-            textAlign: "center",
-            marginBottom: 4,
-          }}
-        ></Typography>
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "center",
-            gap: "3rem",
-            maxWidth: "1200px",
-            margin: "0 auto",
-          }}
-        >
-          {[...Array(6)].map((_, idx) => (
-            <Box
-              key={idx}
-              sx={{
-                height: "328px",
-                width: { xs: "100%", sm: "30%" },
+<div className="flex justify-center items-center px-8 lg:px-20">
+  <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
+    {[...Array(6)].map((_, idx) => (
+      <Box
+        key={idx}
+        sx={{
+          minHeight: "252px",
+          minWidth: "310px",
+          height: { sm:"252px", md:"328px"},
+          width: { sm:"310px", md:"404px"},
+          backgroundImage:
+            "url(https://s3-alpha-sig.figma.com/img/ad8b/a92c/9e252417e19320a3688d65c4c95a8751?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mfeINBu3dY-TvwbTigHC6ZIaK2X0pbBCfjhsnht2n6CzTTVzCBeEKrSWLN03a3kZra0dUUpEMBf302bsw5ufi2RHCe7rqAAh~x8NloLiLRtDwx7cvJLCPAPl4NXeddJWvc7tvGtDSvRt2lPfd3RGpyGbjyf--tfa57vIr51cva4G5M1RNHaiZXPqbuZMkCS6VbqTC1ov9saZv5ygv9u~LQyVu-vxdwvGy2hw0ZqTdglxGRIIoXrUsLOiv9B4u4PbogxW5G20IdVJFyA8mK5jrexwX2p3ENS6fFHSVteYB9CJCmLw22pfKyEyMmWJgbsqczNvp0-nenV507qPXsUstg__)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+    ))}
+  </div>
+</div>
 
-                backgroundImage:
-                  "url(https://s3-alpha-sig.figma.com/img/ad8b/a92c/9e252417e19320a3688d65c4c95a8751?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mfeINBu3dY-TvwbTigHC6ZIaK2X0pbBCfjhsnht2n6CzTTVzCBeEKrSWLN03a3kZra0dUUpEMBf302bsw5ufi2RHCe7rqAAh~x8NloLiLRtDwx7cvJLCPAPl4NXeddJWvc7tvGtDSvRt2lPfd3RGpyGbjyf--tfa57vIr51cva4G5M1RNHaiZXPqbuZMkCS6VbqTC1ov9saZv5ygv9u~LQyVu-vxdwvGy2hw0ZqTdglxGRIIoXrUsLOiv9B4u4PbogxW5G20IdVJFyA8mK5jrexwX2p3ENS6fFHSVteYB9CJCmLw22pfKyEyMmWJgbsqczNvp0-nenV507qPXsUstg__)",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }}
-            />
-          ))}
-        </Box>
-      </Box>
+
 
       <OurStory
         our={tAboutUsOurStory2("small_title")}
@@ -143,31 +128,30 @@ const About = () => {
             sx={{
               fontSize: { xs: "1.25rem", md: "2rem" },
               fontWeight: 500,
-              maxWidth: { md: "750px", lg: "1400px" },
-              px: { xs: "2rem" },
-              marginTop: 2,
+              maxWidth: { sm: "750px", md:"1000px", lg: "1400px" },
+              px: { xs: "2rem", lg:"1rem" },
+              my: 2,
               lineHeight: { md: "2.5rem", xs: "1.8rem" },
               color: "#FFFFFF",
               textTransform: "uppercase",
             }}
           >
-            Werden Sie Teil unseres Netzwerks und gestalten Sie die Zukunft der
-            albanisch-deutschen juristischen Zusammenarbeit mit!
+            {tAboutUsHero("bottom-hero-text")}
           </Typography>
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: "main",
-              ":hover": { backgroundColor: "darkred" },
-              py: 0.8,
-              px: 3,
-              borderRadius: "4px",
-              fontSize: "0.9rem",
-              textTransform: "none",
-              marginTop: 2,
-            }}
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "main",
+            ":hover": { backgroundColor: "darkred" },
+            mt: 1,
+            py: 1.5,
+            px: 5,
+            borderRadius: "5px",
+            fontSize: "1.2rem",
+            textTransform: "none",
+          }}
           >
-            Mitglied werden
+            {tAboutUsHero("become")}
           </Button>
         </Box>
       </Box>
