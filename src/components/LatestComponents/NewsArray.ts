0a78bb264a359adf6e6b3ccf7d089b2f945533fe
nartkosova <nartkosova21@gmail.com>
2024-12-17 11:09:54 +0100

    fixed links for event and news

--- src/components/LatestComponents/NewsArray.ts
+++ src/components/LatestComponents/NewsArray.ts
@@ -1,124 +1,124 @@
 interface blogs {
-    id: number;
-    title: string;
-    topic: string;
-    description: string;
-    image: string;
-    publisher: string;
-    date: string;
-  }
+  id: number;
+  title: string;
+  topic: string;
+  description: string;
+  image: string;
+  publisher: string;
+  date: string;
+}
 
 const Blog: blogs[] = [
-    // {
-    //   id: 1,
-    //   title: "Lorem Ipsum is simply",
-    //   topic: "Topic 1",
-    //   description:
-    //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    //   image:
-    //     "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
-    //   publisher: "Publisher Name",
-    //   date: "2 Dec 2024",
-    // },
-    // {
-    //   id: 2,
-    //   title: "Why Lorem Ipsum?",
-    //   topic: "Topic 2",
-    //   description:
-    //     "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    //   image:
-    //     "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
-    //   publisher: "Publisher Name",
-    //   date: "1 Dec 2024",
-    // },
-    // {
-    //   id: 3,
-    //   title: "The Evolution of Lorem Ipsum",
-    //   topic: "Topic 3",
-    //   description:
-    //     "It has survived not only five centuries, but also the leap into electronic typesetting. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    //   image:
-    //     "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
-    //   publisher: "Publisher Name",
-    //   date: "29 Nov 2024",
-    // },
-    // {
-    //   id: 4,
-    //   title: "Lorem Ipsum is simply",
-    //   topic: "Topic 1",
-    //   description:
-    //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    //   image:
-    //     "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
-    //   publisher: "Publisher Name",
-    //   date: "2 Dec 2024",
-    // },
-    // {
-    //   id: 5,
-    //   title: "Why Lorem Ipsum?",
-    //   topic: "Topic 2",
-    //   description:
-    //     "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    //   image:
-    //     "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
-    //   publisher: "Publisher Name",
-    //   date: "1 Dec 2024",
-    // },
-    // {
-    //   id: 6,
-    //   title: "The Evolution of Lorem Ipsum",
-    //   topic: "Topic 3",
-    //   description:
-    //     "It has survived not only five centuries, but also the leap into electronic typesetting. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    //   image:
-    //     "https://s3-alpha-sig.figma.com/img/2c81/6ac7/41d004aa35514cb231d5634d0473baae?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=p9tSJRQt2-ddeyqa11MMU0FGVgLDEaJVh05leKIa9OFYA7fdEk2SWBTt2oVLKOAK~s~m6waBQWXNn12OqFrJWMi3z8tXaOPhI7Ybugfy9VChn4SjQk3RrO-A3CqXCSx3A7STJFePw5m0QzKwPyzrIEZT0r7YrRve8MNIQhyYGUFniZ7dOvF4KHcGVnombO1Rc4IG18WbX5OyQkTSCouxqtig7AVB2A0waRxxH4RWpW7fxcYpl40Fo9S3RfOWqfNSZQQaaZAXKuFCY3NJtZRYcIhYSRxUvj6lxo8OaWYlKDQ10dWf8PYmyS8V5NuTvC88iy0UFfqzPTMqbi5JC6k5mQ__",
-    //   publisher: "Publisher Name",
-    //   date: "29 Nov 2024",
-    // },
-    // {
-    //   id: 7,
-    //   title: "The way to program",
-    //   topic: "Topic 1",
-    //   description:
-    //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    //   image:
-    //     "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
-    //   publisher: "Publisher Name",
-    //   date: "2 Dec 2024",
-    // },
-    // {
-    //   id: 8,
-    //   title: "Why Lorem Ipsum?",
-    //   topic: "Topic 2",
-    //   description:
-    //     "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    //   image:
-    //     "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
-    //   publisher: "Publisher Name",
-    //   date: "1 Dec 2024",
-    // },
-    // {
-    //   id: 9,
-    //   title: "The Evolution of Lorem Ipsum",
-    //   topic: "Topic 3",
-    //   description:
-    //     "It has survived not only five centuries, but also the leap into electronic typesetting. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    //   image:
-    //     "https://s3-alpha-sig.figma.com/img/2c81/6ac7/41d004aa35514cb231d5634d0473baae?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=p9tSJRQt2-ddeyqa11MMU0FGVgLDEaJVh05leKIa9OFYA7fdEk2SWBTt2oVLKOAK~s~m6waBQWXNn12OqFrJWMi3z8tXaOPhI7Ybugfy9VChn4SjQk3RrO-A3CqXCSx3A7STJFePw5m0QzKwPyzrIEZT0r7YrRve8MNIQhyYGUFniZ7dOvF4KHcGVnombO1Rc4IG18WbX5OyQkTSCouxqtig7AVB2A0waRxxH4RWpW7fxcYpl40Fo9S3RfOWqfNSZQQaaZAXKuFCY3NJtZRYcIhYSRxUvj6lxo8OaWYlKDQ10dWf8PYmyS8V5NuTvC88iy0UFfqzPTMqbi5JC6k5mQ__",
-    //   publisher: "Publisher Name",
-    //   date: "29 Nov 2024",
-    // },
-    // {
-    //   id: 10,
-    //   title: "The Evolution of Lorem Ipsum",
-    //   topic: "Topic 3",
-    //   description:
-    //     "It has survived not only five centuries, but also the leap into electronic typesetting. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    //   image:
-    //     "https://s3-alpha-sig.figma.com/img/2c81/6ac7/41d004aa35514cb231d5634d0473baae?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=p9tSJRQt2-ddeyqa11MMU0FGVgLDEaJVh05leKIa9OFYA7fdEk2SWBTt2oVLKOAK~s~m6waBQWXNn12OqFrJWMi3z8tXaOPhI7Ybugfy9VChn4SjQk3RrO-A3CqXCSx3A7STJFePw5m0QzKwPyzrIEZT0r7YrRve8MNIQhyYGUFniZ7dOvF4KHcGVnombO1Rc4IG18WbX5OyQkTSCouxqtig7AVB2A0waRxxH4RWpW7fxcYpl40Fo9S3RfOWqfNSZQQaaZAXKuFCY3NJtZRYcIhYSRxUvj6lxo8OaWYlKDQ10dWf8PYmyS8V5NuTvC88iy0UFfqzPTMqbi5JC6k5mQ__",
-    //   publisher: "Publisher Name",
-    //   date: "29 Nov 2024",
-    // },
-  ];
+  {
+    id: 1,
+    title: "The albanian German lawyers",
+    topic: "Topic 1",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image:
+      "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
+    publisher: "Publisher Name",
+    date: "2 Dec 2024",
+  },
+  {
+    id: 2,
+    title: "Why Lorem Ipsum?",
+    topic: "Topic 2",
+    description:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image:
+      "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
+    publisher: "Publisher Name",
+    date: "1 Dec 2024",
+  },
+  {
+    id: 3,
+    title: "The Evolution of Lorem Ipsum",
+    topic: "Topic 3",
+    description:
+      "It has survived not only five centuries, but also the leap into electronic typesetting. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image:
+      "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
+    publisher: "Publisher Name",
+    date: "29 Nov 2024",
+  },
+  {
+    id: 4,
+    title: "Lorem Ipsum is simply",
+    topic: "Topic 1",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image:
+      "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
+    publisher: "Publisher Name",
+    date: "2 Dec 2024",
+  },
+  {
+    id: 5,
+    title: "Why Lorem Ipsum?",
+    topic: "Topic 2",
+    description:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image:
+      "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
+    publisher: "Publisher Name",
+    date: "1 Dec 2024",
+  },
+  {
+    id: 6,
+    title: "The Evolution of Lorem Ipsum",
+    topic: "Topic 3",
+    description:
+      "It has survived not only five centuries, but also the leap into electronic typesetting. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image:
+      "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
+    publisher: "Publisher Name",
+    date: "29 Nov 2024",
+  },
+  {
+    id: 7,
+    title: "The way to program",
+    topic: "Topic 1",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image:
+      "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
+    publisher: "Publisher Name",
+    date: "2 Dec 2024",
+  },
+  {
+    id: 8,
+    title: "Why Lorem Ipsum?",
+    topic: "Topic 2",
+    description:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image:
+      "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
+    publisher: "Publisher Name",
+    date: "1 Dec 2024",
+  },
+  {
+    id: 9,
+    title: "The Evolution of Lorem Ipsum",
+    topic: "Topic 3",
+    description:
+      "It has survived not only five centuries, but also the leap into electronic typesetting. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image:
+      "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
+    publisher: "Publisher Name",
+    date: "29 Nov 2024",
+  },
+  {
+    id: 10,
+    title: "The Evolution of Lorem Ipsum",
+    topic: "Topic 3",
+    description:
+      "It has survived not only five centuries, but also the leap into electronic typesetting. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image:
+      "https://s3-alpha-sig.figma.com/img/5d5e/8284/5d3abe2fcad0b544fec8284e9ce0ef49?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWHVImTMTSXsKKSpXs4c1i3v4hNOu7SNGlDJnfSW5rBTULBaMH3T2GRRks-r3blyEDBbEIs5OsLVlUysGAMYsqXnnOHVWUrNafTXSYkyoirMmi30oISZeGyC~3KrUPtGZ9CyjYM-Lq4Alz4deso3UxoCY2PUWOMhKQkN9J3OaPCOf1T14fosec~YNNaL1da34x7NfixFjH9ffB9FpUTpGBAdNGs94bQ0oofhxd-RdFWf6BshDmcYnLlyE~cuCTWtJ5tDHYV5wtuDsiehHgvK75GuEUBuXjtF3IbB50n5R5hSkvBehjprPn~yRW5fX6ivifVz~7kz~EXPDNSvJN9jCQ__",
+    publisher: "Publisher Name",
+    date: "29 Nov 2024",
+  },
+];
 
-  export default Blog
+export default Blog;
